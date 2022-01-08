2e225a91270c21b8690c700bc97aaeac38382514
KIM Se Hyung <tpgud33@gmail.com>
2022-01-08 19:49:04 +0900

    mongodb settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_HOST: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRoot(process.env.MONGO_HOST),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
